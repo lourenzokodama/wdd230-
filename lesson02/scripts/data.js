44232d8067d7c6f580e2850d74856673f7ffb67a
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-09-27 02:55:52 -0600

    Update and rename data.js to dates.js

--- lesson02/scripts/data.js
+++
@@ -1,5 +0,0 @@
-const d = new Date();
-document.getElementById("year").innerHTML = d.getFullYear();
-document.getElementById("time").innerHTML = "Last updated:"+document.lastModified
-
-
---
+++ lesson02/scripts/dates.js
@@ -0,0 +1,9 @@
+// Get the current year and update the element
+const d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
+document.getElementById("time").innerHTML = "Last updated:"+document.lastModified
+
+// Get the last modified date and update the "lastModified" element
+const dateElement = document.getElementById("lastModified");
+const lastModifiedDate = document.lastModified;
+dateElement.textContent = "Last Modified: " + lastModifiedDate;
