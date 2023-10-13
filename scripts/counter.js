aad8ac29d15eb045705bb267460b42d120a7bdf9
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-10-12 20:11:10 -0600

    Update and rename hamburguer.js to counter.js

---
+++ scripts/counter.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+button.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  button.classList.toggle("open");
+
+const visitsDisplay = document.querySelector("#counter");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `This is your first visit.  Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+});
