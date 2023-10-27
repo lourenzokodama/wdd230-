ca544cef1905bd02e7afacf891ac2f1f2b45df4b
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-10-26 21:44:39 -0600

    Update counter.js

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,21 +1,6 @@
-const button = document.querySelector("#menu");
-const nav = document.querySelector("nav");
-
-button.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  button.classList.toggle("open");
-
-const visitsDisplay = document.querySelector("#people");
-
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-if (numVisits !== 0) {
-  visitsDisplay.textContent = numVisits;
-} else {
-  visitsDisplay.textContent = `This is your first visit.  Welcome!`;
-}
-
-numVisits++;
-
-localStorage.setItem("numVisits-ls", numVisits);
-});
+  window.onload = function() {
+      let counter = Number(window.localStorage.getItem("counter")) || 0;
+      counter++;
+      window.localStorage.setItem("counter", counter);
+      document.getElementById("counterDisplay").textContent = counter;
+    };
