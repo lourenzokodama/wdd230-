c70c5265f62a3f467bf1dd2099594b82cae0ced3
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-11-05 23:01:34 -0700

    Delete Apresentation directory

--- Apresentation/scripts/ss-1.js
+++
@@ -1,7 +0,0 @@
-function storeVal() {
-    sessionStorage.setItem('ss_1_message', document.getElementById('session-storage-set').value)
-}
-
-function getVal() {
-    document.getElementById('session-storage-get').value = sessionStorage.getItem('ss_1_message')
-}
