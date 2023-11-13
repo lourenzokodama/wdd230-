a6817b6472099110b98bb2c1270670416fde6676
lourenzokodama <19721070+lourenzokodama@users.noreply.github.com>
2023-11-13 02:30:05 -0700

    Update prophets.js

--- lesson09/prophets.js
+++ lesson09/prophets.js
@@ -1,31 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var prophetsList = [
-        {
-            name: 'Joseph Smith',
-            img: 'images/joseph-smith.jpg',
-            info: 'First President of the Church of Jesus Christ of Latter-day Saints, established the Church, and translated the Book of Mormon.',
-        },
-        // Add more prophets here
-    ];
+const gridbutton = document.querySelector("#grid");
 
-    var listContainer = document.getElementById('prophets-list');
-
-    prophetsList.forEach(function(prophet) {
-        var card = document.createElement('div');
-        card.className = 'card';
-
-        var img = document.createElement('img');
-        img.src = prophet.img;
-        card.appendChild(img);
-
-        var h2 = document.createElement('h2');
-        h2.textContent = prophet.name;
-        card.appendChild(h2);
-
-        var p = document.createElement('p');
-        p.textContent = prophet.info;
-        card.appendChild(p);
-
-        listContainer.appendChild(card);
-    });
+gridbutton.addEventListener("click", () => {
+   	display.classList.add("grid");
 });
+
+listbutton.addEventListener("click", showList); // example using defined function
+function showList() {
+    	display.classList.remove("grid");
+    }
